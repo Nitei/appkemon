9c2b25db9725a2c6d2c9001a216f90495cc84df2
Jorge <jorgecampoy81@gmail.com>
2019-01-22 22:24:20 +0100

    Added stats and moves, all loops in titlecase

--- src/app/poke-stats/poke-stats.page.ts
+++ src/app/poke-stats/poke-stats.page.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PokeService } from '../poke-service/poke.service';
 
-@Component({
+@Component( {
   selector: 'app-poke-stats',
   templateUrl: './poke-stats.page.html',
-  styleUrls: ['./poke-stats.page.sass'],
-})
+  styleUrls: [ './poke-stats.page.sass' ],
+} )
 export class PokeStatsPage implements OnInit {
+  pokeId: string;
+  pokeName: string;
+  pokeData: object;
 
-  constructor() { }
+  constructor ( private route: ActivatedRoute, private servicio: PokeService ) { }
+
+  ionViewWillEnter() {
+    this.pokeId = this.route.snapshot.params.id;
+    this.pokeName = this.route.snapshot.params.nombre;
+
+    this.servicio.getData( `https://pokeapi.co/api/v2/pokemon/${ this.pokeId }` )
+      .subscribe( data => { this.pokeData = data; } );
+  }
 
   ngOnInit() {
   }
